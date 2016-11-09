16ef876b011ca298550d96e0876865f3694535e2
Subhakanta Rout <subhakantrout@gmail.com>
2016-11-09 17:27:59 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://paxanova.in/new/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://paxanova.in/new/');
+
+// DIR
+define('DIR_APPLICATION', '/home/paxanova/public_html/new/catalog/');
+define('DIR_SYSTEM', '/home/paxanova/public_html/new/system/');
+define('DIR_IMAGE', '/home/paxanova/public_html/new/image/');
+define('DIR_LANGUAGE', '/home/paxanova/public_html/new/catalog/language/');
+define('DIR_TEMPLATE', '/home/paxanova/public_html/new/catalog/view/theme/');
+define('DIR_CONFIG', '/home/paxanova/public_html/new/system/config/');
+define('DIR_CACHE', '/home/paxanova/public_html/new/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/paxanova/public_html/new/system/storage/download/');
+define('DIR_LOGS', '/home/paxanova/public_html/new/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/paxanova/public_html/new/system/storage/modification/');
+define('DIR_UPLOAD', '/home/paxanova/public_html/new/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'paxanova_new');
+define('DB_PASSWORD', 'pass@123');
+define('DB_DATABASE', 'paxanova_new');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
